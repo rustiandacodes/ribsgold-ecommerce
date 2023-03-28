f45d96ebcd9486153559ecf592b073abf0fd236d
rustiandacode <rustiandazen09@gmail.com>
2023-03-28 23:01:15 +0700

    edited why people choose use on mobile screen

--- src/components/WhyPeopleChooseUs.jsx
+++ src/components/WhyPeopleChooseUs.jsx
@@ -8,8 +8,8 @@ const WhyPeopleChooseUs = () => {
       <div className="container mx-auto py-16 px-10 lg:px-0">
         <h2 className="text-2xl font-bold text-center">Why People Choose Us</h2>
         <hr className="mb-14 mt-3 mx-auto w-20 border-2 rounded-full border-yellow-400" />
-        <div className="flex justify-between items-center text-center">
-          <div className="w-1/4 flex flex-col items-center">
+        <div className="flex flex-wrap gap-10 justify-between items-center text-center">
+          <div className="md:w-1/4 flex flex-col items-center">
             <CheckSquare color="#fbbf24" size={38} />
             <h3 className="text-lg font-bold my-3">High Quality</h3>
             <p className="text-sm">
@@ -17,7 +17,7 @@ const WhyPeopleChooseUs = () => {
               dispatch them
             </p>
           </div>
-          <div className="w-1/4 flex flex-col items-center">
+          <div className="md:w-1/4 flex flex-col items-center">
             <CornerDownLeft color="#fbbf24" size={38} />
             <h3 className="text-lg font-bold my-3">Easy Return</h3>
             <p className="text-sm">
@@ -25,7 +25,7 @@ const WhyPeopleChooseUs = () => {
               shop
             </p>
           </div>
-          <div className="w-1/4 flex flex-col items-center">
+          <div className="md:w-1/4  flex flex-col items-center">
             <Users color="#fbbf24" size={38} />
             <h3 className="text-lg font-bold my-3">Customer Service</h3>
             <p className="text-sm">
